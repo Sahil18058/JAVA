c7de70311d233232646c49245c41a0f16ba45423
Sahil18058 <156393754+Sahil18058@users.noreply.github.com>
2024-01-12 09:03:10 +0530

    Add files via upload

---
+++ Positivenegative.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Positivenegative {
+    public static void main(String[] args)
+    {
+        byte b1;
+        Scanner scin=new Scanner(System.in);
+        System.out.println("Enter NO :");   
+        b1 =scin.nextByte();
+
+        if(b1>0)
+        {
+            System.out.println("It is Positive Number "); 
+        }
+	else if(b1==0)
+	{
+            System.out.println("It is Neither Negative Nor Positive "); 
+        }
+        else
+        {
+	    System.out.println("It is a Negative Number"); 
+        }
+     }
+}
